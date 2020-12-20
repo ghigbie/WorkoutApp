3ca63e72c38bb1c4d945027c4436f979a69e409b
ghigbie <gehigbie@gmail.com>
2020-12-20 00:46:57 -0500

    Initial commit

---
+++ WorkoutApp/WorkoutAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutAppApp.swift
+//  WorkoutApp
+//
+//  Created by George Higbie on 12/20/20.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
